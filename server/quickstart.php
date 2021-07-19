631cb7de872f1205d80a769accd23ae449dc8960
Its-your-death <VT.Tkachiov2000@yandex.ru>
2021-07-19 20:50:20 +0300

    master

---
+++ server/quickstart.php
@@ -0,0 +1,56 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Content-Type: text/html; charset=utf-8");
+
+// Подключаем клиент Google таблиц
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$JSONData = file_get_contents("php://input");
+$dataObject = json_decode($JSONData);
+
+
+$userName = $dataObject->name;
+$userSurname = $dataObject->surname;
+$userPhone = $dataObject->phone;
+$userEmail = $dataObject->email;
+$userComm = $dataObject->comment;
+
+
+
+
+// Наш ключ доступа к сервисному аккаунту
+$googleAccountKeyFilePath = __DIR__ . '/Key.json';
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $googleAccountKeyFilePath);
+
+// Создаем новый клиент
+$client = new Google_Client();
+// Устанавливаем полномочия
+$client->useApplicationDefaultCredentials();
+
+// Добавляем область доступа к чтению, редактированию, созданию и удалению таблиц
+$client->addScope('https://www.googleapis.com/auth/spreadsheets');
+
+$service = new Google_Service_Sheets($client);
+
+// ID таблицы
+$spreadsheetId = '1J1A3GSNP1q9PmcTOE6qaYkDA6yHC9Lg47PX00dz6KdY';
+$spreadsheetName = "list1";
+
+$values = [
+    [$userName,$userSurname,$userPhone,$userEmail,$userComm]
+];
+
+
+$body = new Google_Service_Sheets_ValueRange(['values'=> $values]);
+
+$option = array('valueInputOption' => 'USER_ENTERED');
+
+$service->spreadsheets_values->append($spreadsheetId, $spreadsheetName, $body, $option);
+
+echo json_encode($userName);
+echo json_encode($userSurname);
+echo json_encode($userPhone);
+echo json_encode($userEmail);
+echo json_encode($userComm);
